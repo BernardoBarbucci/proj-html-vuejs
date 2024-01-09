1b9e68e438658db748d8a9ff61feca1ba441d4ca
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-09 11:50:14 +0100

    macrostruttura AppMusic completata

--- avada-music/src/components/AppMusic.vue
+++ avada-music/src/components/AppMusic.vue
@@ -1,13 +1,26 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <section id="music_life" class="container-fluid">
+        <div class="">
+            <h1 class="text-white fw-bold d-flex justify-content-center">
+                Music if Life
+            </h1>
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fuga veritatis architecto magni laudantium
+                voluptatem velit amet? Tempora perspiciatis, deserunt reprehenderit ex natus aut corrupti. Reprehenderit
+                placeat sed facere. Velit?</p>
+        </div>
+    </section>
 </template>
 <script>
 export default {
     name: 'AppMusic',
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+@import './style/partials/variables.scss';
+
+section {
+    background-color: $bg-color-dark;
+
+}
 </style>
