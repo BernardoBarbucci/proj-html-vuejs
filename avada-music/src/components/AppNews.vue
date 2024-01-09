3ce005990904910d11d048e12161e337a9072dbd
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-09 15:55:57 +0100

    creata section con wrapper per le cards di AppNews

--- avada-music/src/components/AppNews.vue
+++ avada-music/src/components/AppNews.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="sec_title" class="container-fluid">
         <div class="row">
-            <div class="offset-3 col-6 pt-4 mb-3 custom_title my_border">
+            <div class="offset-3 col-6 pt-4 mb-2 custom_title">
                 <h1 class="text-white fw-bold d-flex justify-content-center">
                     Latest Band News
                 </h1>
@@ -13,6 +13,11 @@
                     Magnam, fuga veritatis architecto magnificum laudantium
                     oluptatem velit amet? </p>
             </div>
+        </div>
+    </section>
+    <section id="news" class="container-fluid">
+        <div id="cardsWrapper" class="container col-8 offset-2 my_border">
+
         </div>
     </section>
 </template>
@@ -29,5 +34,15 @@ export default {
 #sec_title {
     background-color: $bg-color-darkest;
 
+}
+
+// fine sass first section
+#news {
+    height: 40rem;
+    background: $bg-color-darkest;
+}
+
+#cardsWrapper {
+    height: 95%;
 }
 </style>
