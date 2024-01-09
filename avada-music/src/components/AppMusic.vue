dce47870402b04300cd92dcafa1db9b05cbd13c6
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-09 12:08:14 +0100

    struttura modifiata con colonne per renderla responsive piu avanti

--- avada-music/src/components/AppMusic.vue
+++ avada-music/src/components/AppMusic.vue
@@ -1,14 +1,20 @@
 <template>
     <section id="music_life" class="container-fluid">
-        <div class="">
-            <h1 class="text-white fw-bold d-flex justify-content-center">
-                Music if Life
-            </h1>
-            <hr />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fuga veritatis architecto magni laudantium
-                voluptatem velit amet? Tempora perspiciatis, deserunt reprehenderit ex natus aut corrupti. Reprehenderit
-                placeat sed facere. Velit?</p>
+        <div class="row">
+            <div class="offset-4 col-4 border">
+                <h1 class="text-white fw-bold d-flex justify-content-center">
+                    Music if Life
+                </h1>
+                <hr />
+            </div>
+            <div class="offset-3 col-6 text-white">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fuga veritatis architecto magni
+                    laudantium
+                    voluptatem velit amet? Tempora perspiciatis, deserunt reprehenderit ex natus aut corrupti. Reprehenderit
+                    placeat sed facere. Velit?</p>
+            </div>
         </div>
+
     </section>
 </template>
 <script>
@@ -21,6 +27,11 @@ export default {
 
 section {
     background-color: $bg-color-dark;
+    height: 15rem;
+}
 
+hr {
+    color: $white;
+    width: 3rem;
 }
 </style>
