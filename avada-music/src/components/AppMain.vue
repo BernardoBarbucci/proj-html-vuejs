9ca4db4c6818ee90f5bdf4fbd1fb40a7b85ac0a9
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-09 11:32:00 +0100

    impostato appmain con i suoi elementi figli

--- avada-music/src/components/AppMain.vue
+++ avada-music/src/components/AppMain.vue
@@ -1,11 +1,27 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <main>
+        <AppMusic />
+        <AppNews />
+        <AppComment />
+        <AppEvents />
+    </main>
 </template>
+
 <script>
+import AppComment from './AppComment.vue';
+import AppEvents from './AppEvents.vue';
+import AppMusic from './AppMusic.vue';
+import AppNews from './AppNews.vue';
+
 export default {
     name: 'AppMain',
+    components: {
+        AppMain,
+        AppComment,
+        AppEvents,
+        AppNews,
+        AppMusic
+    }
 }
 </script>
 <style lang="">
