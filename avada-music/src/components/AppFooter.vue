8248b460994dac96107f86343f612e93ba1c9f21
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-10 00:19:23 +0100

    appEvents completato nel commit precedente + struttura footer creata

--- avada-music/src/components/AppFooter.vue
+++ avada-music/src/components/AppFooter.vue
@@ -1,13 +1,18 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <footer class="container-fluid">
+        <section class="col-12">
+            <div class="footerLogo">
+
+            </div>
+            <div class="footerMenu">
+
+            </div>
+        </section>
+    </footer>
 </template>
 <script>
 export default {
     name: 'AppFooter',
 }
 </script>
-<style lang="">
-    
-</style>
+<style lang="scss"></style>
