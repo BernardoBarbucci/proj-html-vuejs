53d51805290b968bb47961a42f25103b631a3775
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-09 12:22:15 +0100

    AppMusic completato

--- avada-music/src/components/AppMusic.vue
+++ avada-music/src/components/AppMusic.vue
@@ -1,17 +1,15 @@
 <template>
     <section id="music_life" class="container-fluid">
         <div class="row">
-            <div class="offset-4 col-4 border">
+            <div class="offset-4 col-4 pt-3 custom_title">
                 <h1 class="text-white fw-bold d-flex justify-content-center">
-                    Music if Life
+                    Music is Life
                 </h1>
-                <hr />
+                <hr class="mx-auto" />
             </div>
-            <div class="offset-3 col-6 text-white">
+            <div class="offset-3 col-6 text-white text-center mb-3">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fuga veritatis architecto magni
-                    laudantium
-                    voluptatem velit amet? Tempora perspiciatis, deserunt reprehenderit ex natus aut corrupti. Reprehenderit
-                    placeat sed facere. Velit?</p>
+                    laudantium voluptatem velit amet? </p>
             </div>
         </div>
 
@@ -27,11 +25,24 @@ export default {
 
 section {
     background-color: $bg-color-dark;
-    height: 15rem;
+
+    .custom_title {
+        height: 6rem;
+        border-top: .5rem solid $theme-color;
+
+        h1 {
+            font-size: 2.2rem;
+        }
+    }
 }
 
 hr {
     color: $white;
-    width: 3rem;
+    width: 9rem;
+}
+
+p {
+    font-size: .65rem;
+    color: $p-text;
 }
 </style>
