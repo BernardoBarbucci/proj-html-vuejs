337ab474900a2f5ed19379717be33318e07ea17d
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-10 17:13:48 +0100

    rimossi elementi superflui ed importato collapse da bootstrap, il quale ho adattato all'article per creare un effetto simile a quello richiesto

--- avada-music/src/components/AppEvents.vue
+++ avada-music/src/components/AppEvents.vue
@@ -2,40 +2,37 @@
     <section id="sec_title" class="container-fluid">
         <div class="row">
             <div class="offset-4 col-4 pt-4 mb-1 custom_title">
-                <h1 class="text-white fw-bold d-flex justify-content-center">
-                    Live Dates
-                </h1>
+                <h1 class="text-white fw-bold d-flex justify-content-center">Live Dates</h1>
                 <hr class="mx-auto custom_hr" />
             </div>
             <div class="offset-3 col-6 text-white text-center mb-3">
                 <p id="sec_parag" class="crimson-font">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Magnam, fuga veritatis architecto magnificum laudantium
-                    oluptatem velit amet? </p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fuga veritatis architecto magnificum
+                    laudantium
+                    oluptatem velit amet?
+                </p>
             </div>
         </div>
     </section>
+
     <!-- events -->
     <section id="events" class="container-fluid">
-
         <div class="col-10 offset-1">
-
             <article v-for="(event, index) in events" :key="index" class="col-12 d-flex align-items-center custom_event"
-                @click="toggleEventSize(index)" :class="{ expanded: event.expanded }">
+                data-bs-toggle="collapse" :data-bs-target="'#eventCollapse' + index" aria-expanded="false"
+                aria-controls="'eventCollapse' + index">
                 <p class="mx-2 text-white">+</p>
-                <p class="event_name text-uppercase">
-                    {{ event.name }}
-                </p>
+                <p class="event_name text-uppercase">{{ event.name }}</p>
+                <div :id="'eventCollapse' + index" class="collapse custom-collapse" :class="{ 'show': event.expanded }">
+                    <p style="height: 10rem;">hello</p>
+                </div>
             </article>
-
         </div>
-
     </section>
+
     <!-- latest news -->
     <section class="latestNews d-flex align-items-center justify-content-center">
-        <p class="text-black mb-0">
-            VIEW ALL LIVE DATES
-        </p>
+        <p class="text-black mb-0">VIEW ALL LIVE DATES</p>
     </section>
 </template>
 
@@ -51,11 +48,6 @@ export default {
                 { name: 'Moga Festival - Essaouria, Morocco', expanded: false },
                 { name: 'Envision Festival - Uvita, Costa Rica', expanded: false }
             ]
-        };
-    },
-    methods: {
-        toggleEventSize(index) {
-            this.$set(this.events, index, { ...this.events[index], expanded: !this.events[index].expanded });
         }
     }
 }
@@ -72,27 +64,20 @@ export default {
     background-color: $bg-color-darkest;
     padding-bottom: 2rem;
 
-    div {
-        .custom_event {
-            background-color: $bg-color-dark;
-            min-height: 2rem;
-            margin-bottom: .5rem;
-            cursor: pointer;
-            transition: height 0.3s linear;
+    .custom_event {
+        background-color: $bg-color-dark;
+        min-height: 2rem;
+        margin-bottom: .5rem;
+        cursor: pointer;
+        transition: height 0.3s linear;
 
-            p {
-                margin-bottom: 0;
-            }
-
-            .event_name {
-                font-size: .75rem;
-                color: $theme-color;
-            }
+        p {
+            margin-bottom: 0;
         }
 
-        // Aggiunto il blocco CSS per l'espansione
-        .expanded {
-            height: 8rem;
+        .event_name {
+            font-size: .75rem;
+            color: $theme-color;
         }
     }
 }
